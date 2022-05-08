1ffd3f24c95449057e45f92938dca105fc3c45bf
aleighsb <aleighsb@users.noreply.github.com>
2022-05-08 03:11:18 -0500

    Update data_cleaning.py

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,17 +8,16 @@ Author: Amber Shannon Barbosa
 import pandas as pd
 import numpy as np
 
+# get and convert Levels FYI json data to csv
 with open('levelsfyi.json', encoding='utf-8') as inputfile:
     df = pd.read_json(inputfile)
 
 df.to_csv('levelsfyi.csv', encoding='utf-8', index=False)
 
-df = df.drop(['cityid', 'rowNumber'], axis = 1)
-
-df.head(5)
-df.tail(5)
-df.title.value_counts()
-
+# drop variables
+# otherdetails seems to have been corrupted
+# cityid, rowNumber, dmaid will not be used in this analysis
+df = df.drop(['cityid', 'rowNumber', 'otherdetails', 'dmaid'], axis = 1)
 df = df.replace("", np.nan)
 
 # delete rows based on condition in column
@@ -33,13 +32,3 @@ df['state'] = df['location'].apply(lambda x: x.split(', ')[1])
 
 # prior experience = Years of experience - years at company
 df['priorXP'] = df['yearsofexperience']-df['yearsatcompany']
-
-# parsing out otherdetails variable
-# change otherdetails variable to string
-df['otherdetails'] = df['otherdetails'].astype(str)
-# bonus
-df['bonus'] = df['otherdetails'].apply(lambda x: 1 if 'bonus' in x.lower() else 0)
-df['bonus'].value_counts()
-# sign on, signon, sign-on, signing
-df['sign on'] = df['otherdetails'].apply(lambda x: 1 if 'sign on' in x.lower() or 'signon' in x.lower() or 'sign-on' in x.lower() or 'signing' in x.lower() else 0)
-df['sign on'].value_counts()
